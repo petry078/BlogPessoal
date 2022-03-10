c0a807e976d67d1715ce88e6dae181e766c488b8
petry078@gmail.com <petry078@gmail.com>
2022-03-10 17:21:52 -0300

    deploy

--- src/main/java/org/generation/blogPessoal/seguranca/BasicSecurityConfig.java
+++ src/main/java/org/generation/blogPessoal/seguranca/BasicSecurityConfig.java
@@ -20,6 +20,7 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService);
+		auth.inMemoryAuthentication().withUser("admin").password(passwordEncoder().encode("admin1")).authorities("ROLE_ADMIN");
 	}
 	
 	@Bean
